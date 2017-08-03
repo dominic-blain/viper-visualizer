ce4cd21f3da0fda3c715c322089a58440e40e057
Dominic Blain <dominic.blain@gmail.com>
2017-08-03 14:30:59 -0400

    Add active state to font inputs

--- app/components/ToolbarInputFont.js
+++ app/components/ToolbarInputFont.js
@@ -4,6 +4,10 @@ import { loadGoogleFont } from '../utils';
 class ToolbarInputFont extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			method: '',
+			uploadName: 'No file chosen'
+		}
 		this.handleOptionChange = this.handleOptionChange.bind(this);
 	}
 
@@ -11,27 +15,29 @@ class ToolbarInputFont extends React.Component {
 		var _this = this;
 		if (event.target.dataset.method == 'google-font') {
 			loadGoogleFont(this.props.name, event.target.value);
+			this.setState({method: 'google-font'});
 			this.props.onOptionChange(event.target.value, this.props.name);
 		}
 		else if (event.target.dataset.method == 'upload') {
 			var files = event.target.files;
 			if	(files.length) {
-			var fontname = files[0].name;
+				var fontname = files[0].name;
 
-			var reader = new FileReader();
-			reader.onload = function (event) {
-				var name = fontname.split('.')[0];
-				var extension = fontname.split('.')[1];
-				var head = document.getElementsByTagName('head')[0];
-				var style = document.createElement('style');
-				style.type = "text/css";
-				style.id = "local-font";
-				style.textContent = "@font-face { font-family: \""+ name +"\"; src: url('"+event.target.result+"'); }";
-				head.appendChild(style);
-				_this.props.onOptionChange(name, _this.props.name);
+				var reader = new FileReader();
+				reader.onload = function (event) {
+					var name = fontname.split('.')[0];
+					var extension = fontname.split('.')[1];
+					var head = document.getElementsByTagName('head')[0];
+					var style = document.createElement('style');
+					style.type = "text/css";
+					style.id = "local-font";
+					style.textContent = "@font-face { font-family: \""+ name +"\"; src: url('"+event.target.result+"'); }";
+					head.appendChild(style);
+					_this.setState({method: 'upload', uploadName: fontname});
+					_this.props.onOptionChange(name, _this.props.name);
+				}
+				reader.readAsDataURL(files[0]);
 			}
-			reader.readAsDataURL(files[0]);
-		}
 		}
 	}
 
@@ -44,13 +50,18 @@ class ToolbarInputFont extends React.Component {
 				</option>
 			);
 		})
+
+		var method = this.state.method;
+		var googleFontActiveClass = (method == 'google-font') ? 'is-active':'';
+		var uploadFontActiveClass = (method == 'upload') ? 'is-active':'';
+
 		return (
 			<div className="toolbar-input-font">
 				<label>
 					<div className="toolbar-option-label">
 						{this.props.label}
 					</div>
-					<div className="toolbar-option-select-ctn">
+					<div className={'toolbar-option-select-ctn ' + googleFontActiveClass}>
 						<select name={this.props.name} data-method="google-font" defaultValue="default" onChange={this.handleOptionChange}>
 							<option value="default"> Choose Google font</option>
 							{items}
@@ -58,12 +69,12 @@ class ToolbarInputFont extends React.Component {
 					</div>
 				</label>
 				<div className="separator"> or </div>
-				<label className="input-font-file-ctn">
+				<label className={'input-font-file-ctn ' + uploadFontActiveClass}>
 					<span className="input-font-file-button">
 						upload font (zip)
 					</span>
 					<span className="input-font-file-filename">
-						No file chosen
+						{this.state.uploadName}
 					</span>
 					<input type="file" data-method="upload" onChange={this.handleOptionChange} />
 				</label>
