98f5d561da812e0da225dcfbdf12c7b00001c70f
Dominic Blain <dominic.blain@gmail.com>
2017-08-03 17:12:14 -0400

    Reset font inputs on change and add roadmap to readme.md

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Viper Visualizer
 Tool to allow visualisation of a layout based on a set of options.
 
+##Roadmap
+###0.2
+- [x] Design toolbar interface
+- [x] Integrate solution for google font and local font upload
+
+###0.3
+- [ ] Add color input component
+- [ ] Add all basic modules to article and correspongin CSS
+
+###0.4
+- [ ] Add save feature to toolbar settings
+- [ ] Create production build process
+
+###0.5
+- [ ] Redesign and refactor input font
+
+...
+
+###1.0
+- [ ] Add app loading screen
+- [ ] Fine-tune before launch
+
 ## Configuration
 create a `config.js` file inside the app folder with the following content:
 ```
--- app/components/ToolbarInputFont.js
+++ app/components/ToolbarInputFont.js
@@ -6,6 +6,7 @@ class ToolbarInputFont extends React.Component {
 		super(props);
 		this.state = {
 			method: '',
+			googleFontValue: 'default',
 			uploadName: 'No file chosen'
 		}
 		this.handleOptionChange = this.handleOptionChange.bind(this);
@@ -15,7 +16,7 @@ class ToolbarInputFont extends React.Component {
 		var _this = this;
 		if (event.target.dataset.method == 'google-font') {
 			loadGoogleFont(this.props.name, event.target.value);
-			this.setState({method: 'google-font'});
+			this.setState({method: 'google-font', googleFontValue: event.target.value, uploadName: 'No file chosen'});
 			this.props.onOptionChange(event.target.value, this.props.name);
 		}
 		else if (event.target.dataset.method == 'upload') {
@@ -26,6 +27,7 @@ class ToolbarInputFont extends React.Component {
 				var reader = new FileReader();
 				reader.onload = function (event) {
 					var name = fontname.split('.')[0];
+					var displayName = name.replace('-', ' ');
 					var extension = fontname.split('.')[1];
 					var head = document.getElementsByTagName('head')[0];
 					var style = document.createElement('style');
@@ -33,7 +35,7 @@ class ToolbarInputFont extends React.Component {
 					style.id = "local-font";
 					style.textContent = "@font-face { font-family: \""+ name +"\"; src: url('"+event.target.result+"'); }";
 					head.appendChild(style);
-					_this.setState({method: 'upload', uploadName: fontname});
+					_this.setState({method: 'upload', uploadName: displayName, googleFontValue:'default'});
 					_this.props.onOptionChange(name, _this.props.name);
 				}
 				reader.readAsDataURL(files[0]);
@@ -62,7 +64,7 @@ class ToolbarInputFont extends React.Component {
 						{this.props.label}
 					</div>
 					<div className={'toolbar-option-select-ctn ' + googleFontActiveClass}>
-						<select name={this.props.name} data-method="google-font" defaultValue="default" onChange={this.handleOptionChange}>
+						<select name={this.props.name} data-method="google-font" value={this.state.googleFontValue} onChange={this.handleOptionChange}>
 							<option value="default"> Choose Google font</option>
 							{items}
 						</select>
