ffa621ae5f2ee36dd62bb7b0b35b6d68b0e55512
Dominic Blain <dominic.blain@gmail.com>
2018-01-31 14:07:41 -0500

    Clean up apiMiddleware residues

--- app/store/store.js
+++ app/store/store.js
@@ -1,7 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducers/reducer';
 import thunk from 'redux-thunk';
-// import apiMiddleware from '../middlewares/apiMiddleware';
 import loggerMiddleware from '../middlewares/loggerMiddleware';
 
 const store = createStore(reducer, applyMiddleware(loggerMiddleware, thunk));
